a7d5df72420889842a2ff9cb0e41c55f97d92e62
Shreyas Walde <waldeshreyas@gmail.com>
2025-09-20 11:16:51 +0530

    merge sort

--- 6.sorting/insertion_sort.py
+++ 6.sorting/insertion_sort.py
@@ -6,9 +6,9 @@ a =  [9,5,3,2,1,6,2,4]
 def insertion_sort(arr):
     n = len(arr) # 8
 
-    for i in range(1,n):
-        for j in range(i,0,-1):
-            if arr[j-1] > arr[j]:
+    for i in range(1,n):     # index from 1 to n=7
+        for j in range(i,0,-1): # index j 7 - 1 
+            if arr[j-1] > arr[j]: # 9 > 5
                 arr[j-1], arr[j] = arr[j], arr[j-1]
             else:
                 break
---
+++ 6.sorting/merge_sort.py
@@ -0,0 +1,53 @@
+# Merge Sort
+# Time: O(n log n)
+# Space: O(n) - Note: can be Log n, but this is harder to write
+
+A = [-5, 3, 2, 1, -3, -3, 7, 2, 2]
+#     0  1  2  3   4   5  6  7  8
+def merge_sort(arr):
+    n = len(arr) # 9 
+
+    if n == 1:
+        return arr
+# Divide    
+
+    m = n // 2 # m = middle point
+    L = arr[:m]  # up untill m (from begining - excluding m)
+    R = arr[m:] #including m till the end.
+
+    L = merge_sort(L)
+    R = merge_sort(R)
+
+    l,r = 0,0  # # Pointers for L and R
+    L_len = len(L)
+    R_len = len(R)
+
+    sorted_arr = [0] * n    # hash?  0(n)
+    
+# Merge    
+    i = 0
+#         0 < 3         0 < 2
+    while l < L_len and r < R_len:
+        if L[l] < R[r]:
+            sorted_arr[i] = L[l]   # choose L
+            l+=1
+        else:
+            sorted_arr[i] = R[r]   # choose R
+            r+=1
+
+        i+=1
+
+    while l < L_len:
+        sorted_arr[i] = L[l]
+        l+=1
+        i+=1
+
+    while r < R_len:
+        sorted_arr[i] = R[r]
+        r+=1
+        i+=1
+
+    return sorted_arr
+
+print("Before:",A)
+print("After: ",merge_sort(A))
